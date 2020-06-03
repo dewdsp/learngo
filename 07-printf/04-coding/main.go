1289bd3dc374fcf7b1ed40e7e7693b0164d4630d
dewdsp <sitthakarn.p@gmail.com>
2020-06-03 20:05:02 +0700

    add from master

---
+++ 07-printf/04-coding/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		planet   = "Venus"
+		distance = 261
+		orbital  = 224.701
+		hasLife  = false
+	)
+
+	fmt.Printf("Planet: %v\n", planet)
+	fmt.Printf("Distance: %v millions kms\n", distance)
+	fmt.Printf("Orbital Period: %v days\n", orbital)
+	fmt.Printf("Does %v have life? %v\n", planet, hasLife)
+
+	fmt.Printf(
+		"%v is %v away, Think! %[2]v kms! %[1]v OMG.\n",
+		planet, distance,
+	)
+
+	fmt.Printf("Orbital Period: %f days\n", orbital)
+	fmt.Printf("Orbital Period: %.0f days\n", orbital)
+	fmt.Printf("Orbital Period: %.1f days\n", orbital)
+	fmt.Printf("Orbital Period: %.2f days\n", orbital)
+}
